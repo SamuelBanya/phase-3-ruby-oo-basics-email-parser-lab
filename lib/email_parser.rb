08cbdcf411f204000af62c9566f278b8eafb66fa
Samuel Banya <sbanya@fastmail.com>
2022-08-22 20:39:07 -0400

    commit tree b276274a59a3add0e386fe5177f0d78811670846

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,21 @@
-# Build a class EmailAddressParser that accepts a string of unformatted 
+# Build a class EmailAddressParser that accepts a string of unformatted
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+  attr_accessor :email_addresses
+
+  def initialize(email_addresses)
+    @email_addresses = email_addresses
+  end
+
+  def parse
+    # ... This pattern was never shown unfortunately, so this was an awkward find
+    # only to figure out that this is basically a glorified regex hunt:
+    email_addresses.split(/, | /).uniq
+    # email_addresses.split(" ")
+    # email_addresses.split(",")
+    # email_addresses = email_addresses.uniq
+  end
+end
